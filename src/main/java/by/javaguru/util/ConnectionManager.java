d51ff6e4aba5c11bc2f65472d126fdfe5dc26c11
Evgeniy <paut.evg@yandex.ru>
2023-11-17 17:38:19 +0300

    refactor code

--- src/main/java/by/javaguru/util/ConnectionManager.java
+++ src/main/java/by/javaguru/util/ConnectionManager.java
@@ -6,7 +6,10 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static Connection getConnection(String url, String username, String password, String schema) {
+    public static Connection getConnection(String url,
+                                           String username,
+                                           String password,
+                                           String schema) {
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             connection.setSchema(schema);
